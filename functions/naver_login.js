7ca11c659d53955440fa93cd32ac7d75f0905f05
soonger3306 <soonger3306@daum.net>
2023-12-17 22:57:23 +0900

    feat:naver_login(firebase)

--- functions/naver_login.js
+++ functions/naver_login.js
@@ -1,75 +1,119 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const axios = require("axios");
-const Async = require("async");
-admin.initializeApp();
+// const functions = require("firebase-functions");
+// const admin = require("firebase-admin");
 
-const naverRequestMeUrl = "https://openapi.naver.com/v1/nid/me";
-exports.createFirebaseToken = functions.https.onRequest((req, res) => {
-  const naverAccessToken = req.body.token;
+// require("dotenv").config();
 
-  Async.waterfall(
-    [
-      (next) => {
-        axios
-          .get(naverRequestMeUrl, {
-            headers: { Authorization: "Bearer " + naverAccessToken },
-          })
-          .then((result) => {
-            const body = result.data.response;
-            const userId = `naver:${body.id}`;
-            const nickname = null;
+// let serviceAccount = {
+//   type: process.env.TYPE,
+//   project_id: process.env.PROJECT_ID,
+//   private_key_id: process.env.PRIVATE_KEY_ID,
+//   private_key: process.env.PRIVATE_KEY,
+//   client_email: process.env.CLIENT_EMAIL,
+//   client_id: process.env.CLIENT_ID,
+//   auth_uri: process.env.AUTH_URI,
+//   token_uri: process.env.TOKEN_URI,
+//   auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X_CERT_URL,
+//   client_x509_cert_url: process.env.CLIENT_X_CERT_URL,
+// };
 
-            const updateParams = {
-              uid: userId,
-              email: body.email,
-              provider: "NAVER",
-              displayName: body.nickname || body.email,
-              photoURL: body.profile_image,
-            };
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://honeytip-deb2e.firebaseio.com",
+// });
 
-            next(null, updateParams);
-          })
-          .catch((error) => {
-            res.status(404).send({
-              message: "There was no user with the given access token.",
-            });
-          });
-      },
-      (userRecord, next) => {
-        admin
-          .auth()
-          .getUserByEmail(userRecord.email)
-          .then((userRecord) => {
-            next(null, userRecord);
-          })
-          .catch((error) => {
-            admin
-              .auth()
-              .createUser(userRecord)
-              .then((user) => {
-                next(null, user);
-              });
-          });
-      },
-      (userRecord, next) => {
-        const userId = userRecord.uid;
-        admin
-          .auth()
-          .createCustomToken(userId, { provider: "NAVER" })
-          .then((token) => {
-            next(null, token);
-          });
-      },
-    ],
-    (err, token) => {
-      if (err) {
-        res
-          .status(500)
-          .send({ error: "Error creating custom token", details: err });
-      } else {
-        res.send({ token });
-      }
-    }
-  );
-});
+// const request = require("request-promise");
+
+// const naverRequestMeUrl = "https://openapi.naver.com/v1/nid/me";
+
+// function requestMe(naverAccessToken) {
+//   console.log("Requesting user profile from Naver API server.");
+//   return request({
+//     method: "GET",
+//     headers: {
+//       Authorization: "Bearer " + naverAccessToken,
+//       "X-Naver-Client-Id": "YYnLXjkdfl6W1vWNfOel",
+//       "X-Naver-Client-Secret": "GtJShppKea",
+//     },
+//     url: naverRequestMeUrl,
+//   });
+// }
+
+// function updateOrCreateUser(userId, email, displayName, photoURL) {
+//   console.log("updating or creating a firebase user");
+//   const updateParams = {
+//     provider: "NAVER",
+//     displayName: displayName,
+//   };
+//   if (displayName) {
+//     updateParams["displayName"] = displayName;
+//   } else {
+//     updateParams["displayName"] = email;
+//   }
+//   if (photoURL) {
+//     updateParams["photoURL"] = photoURL;
+//   }
+//   console.log(updateParams);
+//   return admin
+//     .auth()
+//     .updateUser(userId, updateParams)
+//     .catch((error) => {
+//       if (error.code === "auth/user-not-found") {
+//         updateParams["uid"] = userId;
+//         if (email) {
+//           updateParams["email"] = email;
+//         }
+//         return admin.auth().createUser(updateParams);
+//       }
+//       throw error;
+//     });
+// }
+
+// function createFirebaseToken(naverAccessToken) {
+//   return requestMe(naverAccessToken)
+//     .then((response) => {
+//       const body = JSON.parse(response);
+//       console.log(body);
+//       const userId = `naver:${body.response.id}`;
+//       if (!userId) {
+//         return res
+//           .status(404)
+//           .send({ message: "There was no user with the given access token." });
+//       }
+//       let name = null;
+//       let profileImage = null;
+//       if (body.properties) {
+//         name = body.response.name;
+//         profileImage = body.response.profile_image;
+//       }
+//       return updateOrCreateUser(
+//         userId,
+//         body.response.email,
+//         name,
+//         profileImage
+//       );
+//     })
+//     .then((userRecord) => {
+//       const userId = userRecord.uid;
+//       console.log(`creating a custom firebase token based on uid ${userId}`);
+//       return admin.auth().createCustomToken(userId, { provider: "KAKAO" });
+//     });
+// }
+
+// exports.naverCustomAuth = functions
+//   .region("us-central1")
+//   .https.onRequest((req, res) => {
+//     const token = req.body.token;
+//     if (!token)
+//       return res
+//         .status(400)
+//         .send({ error: "There is no token." })
+//         .send({ message: "Access token is a required parameter." });
+
+//     console.log(`Verifying naver token: ${token}`);
+//     createFirebaseToken(token).then((firebaseToken) => {
+//       console.log(`Returning firebase token to user: ${firebaseToken}`);
+//       res.send({ firebase_token: firebaseToken });
+//     });
+
+//     return;
+//   });
